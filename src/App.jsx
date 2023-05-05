784a79a419fc14c78c6f6f7e52fc7f401c1a5699
Rodolfo Echenique <echeniquer@gmail.com>
2023-05-05 00:08:24 -0300

    mejoras css

--- src/App.jsx
+++ src/App.jsx
@@ -32,14 +32,14 @@ export const App = () => {
   return (
     <>
       <h1 className="is-size-1 has-text-centered foldit">Rick and Morty API</h1>
-      <nav className="pagination is-right" role="navigation" aria-label="pagination">
+      <nav className="pagination is-right padding-lateral" role="navigation" aria-label="pagination">
         { (page != 1)? <a className="pagination-previous button is-primary" onClick={prevPage}>Anterior</a> : <a className="pagination-previous is-disabled">Anteror</a> }
         { (page != pages)? <a className="pagination-next button is-link" onClick={nextPage}>Siguiente</a>: <a className="pagination-next is-disabled">Siguiente</a> }
       </nav>
-      <div className="columns margin-top">
+      <div className="columns margin-top padding-lateral">
         <CharacterList characters={characters}/>
       </div>
-      <nav className="pagination is-right" role="navigation" aria-label="pagination">
+      <nav className="pagination is-right padding-lateral" role="navigation" aria-label="pagination">
         { (page != 1)? <a className="pagination-previous button is-primary" onClick={prevPage}>Anteror</a> : <a className="pagination-previous is-disabled">Anteror</a> }
         { (page != pages)? <a className="pagination-next button is-link" onClick={nextPage}>Siguiente</a>: <a className="pagination-next is-disabled">Siguiente</a> }
       </nav>
