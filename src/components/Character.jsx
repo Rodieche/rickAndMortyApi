1436cad33656424bc15076d8387d28cc8965d603
Rodolfo Echenique <echeniquer@gmail.com>
2023-05-07 00:04:23 -0300

    se añade Fontawesome y se mejora las tarjetas

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -16,8 +16,18 @@ export const Character = ({character}) => {
                     </div>
                     <div className="media-content">
                         <p className="title is-4">{character.name}</p>
-                        <p className="subtitle is-6 text-overflow">{character.species}</p>
-                        <p className="subtitle is-6 text-overflow">{character.origin.name}</p>
+                        <p className="subtitle is-6 text-overflow">
+                            <span class="icon">
+                                <i class="fas fa-dragon"></i>
+                            </span>
+                            {character.species}
+                        </p>
+                        <p className="subtitle is-6 text-overflow">
+                            <span class="icon">
+                                <i class="fas fa-earth-americas"></i>
+                            </span>
+                            {character.origin.name}
+                        </p>
                     </div>
                 </div>
             </div>
